820cffae4a302808bfa3b697c12389d4ca0589c2
dougwells <dougwells@gmail.com>
2015-12-07 20:39:50 -0700

    L90:  Keeping straight when JS is running on server vs in the browser.  See notes

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,6 @@ angular.module("TestApp", []);
 angular.module("TestApp").controller("MainController", ctrlFunction);
 
 function ctrlFunction(){
-	this.message = "Hello From AngularJS";
-	
-	this.people = [
-		{name: "John Doe"},
-		{name: "Jane Doe"},
-		{name: "Jim Doe"}
-	]
+	this.people = clientPeople;
+
 }
